c4a62f1ed59982eab0b9f0a411ce460126d0f289
Sidipotu Ashok <sashok@codeaurora.org>
2012-03-30 18:24:01 +0530

    omxcore: Disable MP3 & AAC Hardware decoders for 7x27a.
    Disable hardware decoders as Low Power Audio uses software
    decoders.
    
    Change-Id: Icc0a6df6c6097bb14bc8f119ce50e94cfacc685c

--- omxcore/src/7627A/qc_registry_table_android.c
+++ omxcore/src/7627A/qc_registry_table_android.c
@@ -278,39 +278,7 @@ omx_core_cb_type core[] =
       "video_encoder.avc"
     }
   },
-  {
-    "OMX.qcom.audio.decoder.mp3",
-    NULL, // Create instance function
-    // Unique instance handle
-    {
-      NULL,
-      NULL,
-      NULL,
-      NULL
-    },
-    NULL,   // Shared object library handle
-    "libOmxMp3Dec.so",
-    {
-      "audio_decoder.mp3"
-    }
-  },
-  {
-    "OMX.qcom.audio.decoder.aac",
-    NULL, // Create instance function
-    // Unique instance handle
-    {
-      NULL,
-      NULL,
-      NULL,
-      NULL
-    },
-    NULL,   // Shared object library handle
-    "libOmxAacDec.so",
-    {
-      "audio_decoder.aac"
-    }
-  },
-  {
+ {
     "OMX.qcom.audio.decoder.Qcelp13Hw",
     NULL, // Create instance function
     // Unique instance handle
